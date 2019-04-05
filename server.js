593999a229c1027cdd0139a8ba052fb4eb80660d
Kai Tait <kai.tait@rewe-digital.com>
2019-04-05 21:19:35 +0200

    Adding more content to the modals

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const favicon = require('serve-favicon');
 
 const server = app
     .listen(port, () => {
@@ -14,7 +15,12 @@ const server = app
 module.exports = server;
 
 app.use(express.static("public"));
+app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
